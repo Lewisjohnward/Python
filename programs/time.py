ebdd3cdef0e6cb9c83fb53000abb0789d79498a2
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-01-03 19:08:28 +0000

    added time program

---
+++ programs/time.py
@@ -0,0 +1,14 @@
+"""
+Prints current time to stdout
+"""
+
+import time
+
+
+def main():
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print(current_time)
+
+
+main()
