5d70e42d309e6a7a9bc0e0a520a294c3ac721e2d
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-01-09 15:43:16 +0000

    added QApplication

---
+++ qt/009barebones/barebones.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import (
+        QApplication,
+        QMainWindow,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
